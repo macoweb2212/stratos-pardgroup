681e6aad9675910cfbf433114792cb4968411ff2
LnrdRmj <leonardo.ra14@icloud.com>
2025-05-05 10:28:05 +0200

    api client

---
+++ app/lib/apiClient.ts
@@ -0,0 +1,16 @@
+async function fetchAccessToken() {
+    try {
+        const response = await fetch("/api/get-access-token", {
+            method: "POST",
+        });
+        const token = await response.text();
+
+        console.log("Access Token:", token); // Log the token to verify
+
+        return token;
+    } catch (error) {
+        console.error("Error fetching access token:", error);
+    }
+
+    return "";
+}
